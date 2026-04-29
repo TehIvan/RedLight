deebb2b2cbdd07a1d0fb2df0d285cbc3e4b3462c
TehIvan <83208329+TehIvan@users.noreply.github.com>
2026-04-29 16:41:43 +0200

    Add chat utility method

--- src/main/java/me/ivan/RedLight/RedLightPlugin.java
+++ src/main/java/me/ivan/RedLight/RedLightPlugin.java
@@ -3,6 +3,8 @@ package me.ivan.RedLight;
 import co.aikar.commands.PaperCommandManager;
 import me.ivan.RedLight.command.RedLightCommand;
 import me.ivan.RedLight.listener.PlayerMovement;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -44,5 +46,11 @@ public final class RedLightPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public Component normal(String message) {
+        return MiniMessage.miniMessage().deserialize(
+                this.getConfig().getString("lang.prefix") + this.getConfig().getString("lang.color-normal") + message
+        );
     }
 }
