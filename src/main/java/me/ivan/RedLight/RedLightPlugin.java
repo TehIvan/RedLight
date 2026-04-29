f09a0c9508f008332027a34cfa8d7b1ee49f1b91
TehIvan <83208329+TehIvan@users.noreply.github.com>
2026-04-29 16:49:55 +0200

    Reset Project

--- src/main/java/me/ivan/RedLight/RedLightPlugin.java
+++ src/main/java/me/ivan/RedLight/RedLightPlugin.java
@@ -1,18 +1,19 @@
 package me.ivan.RedLight;
 
 import co.aikar.commands.PaperCommandManager;
+import lombok.Getter;
 import me.ivan.RedLight.command.RedLightCommand;
-import me.ivan.RedLight.listener.PlayerMovement;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
+@Getter
 public final class RedLightPlugin extends JavaPlugin {
 
-    public Game game;
-    public Economy economy;
+    private Economy economy;
 
     public void setupEconomy() {
         if (getServer().getPluginManager().getPlugin("Vault") == null) {
@@ -25,6 +26,7 @@ public final class RedLightPlugin extends JavaPlugin {
         economy = rsp.getProvider();
         return;
     }
+
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -32,15 +34,12 @@ public final class RedLightPlugin extends JavaPlugin {
         setupEconomy();
         this.saveDefaultConfig();
 
-        this.game = new Game(this);
-
         PaperCommandManager manager = new PaperCommandManager(this);
 
         manager.registerCommand(
                 new RedLightCommand(this)
         );
 
-        this.getServer().getPluginManager().registerEvents(new PlayerMovement(this), this);
     }
 
     @Override
--- src/main/java/me/ivan/RedLight/command/RedLightCommand.java
+++ src/main/java/me/ivan/RedLight/command/RedLightCommand.java
@@ -28,47 +28,5 @@ public class RedLightCommand extends BaseCommand {
         );
     }
 
-//    @Subcommand("join")
-//    public void joinGame(Player player) {
-//        this.plugin.game.addPlayer(player);
-//        Util.send(player, "&8[&6Devnics&8] &7You are now in the waiting lobby for &cRedLight &aGreenLight");
-//    }
-//
-//    @Subcommand("setrounds")
-//    @CommandPermission("red.use")
-//    public void setRounds(Player player, @Name("rounds") Integer num) {
-//        this.plugin.getConfig().set("rounds", num);
-//        this.plugin.saveConfig();
-//
-//        Util.send(player, "&8[&6Devnics&8] &aSet the number of rounds to &d{}".replace("{}", Integer.toString(num)));
-//    }
-//
-//    @Subcommand("setspawn")
-//    @CommandPermission("red.use")
-//    public void setSpawn(Player player) {
-//        this.plugin.getConfig().set("arena.spawn", player.getLocation());
-//        this.plugin.saveConfig();
-//
-//        Util.send(player, "&8[&6Devnics&8] &aSet arena spawn!");
-//    }
-//
-//    @Subcommand("start")
-//    @CommandPermission("red.use")
-//    public void startGame(CommandSender sender) {
-//        this.plugin.game.startGame();
-//    }
-//
-//    @Subcommand("light")
-//    @CommandPermission("red.use")
-//    public void ChangeLight(CommandSender sender) {
-//        this.plugin.game.setRedLight(!this.plugin.game.isRedLight);
-//        Util.send(sender, "&8[&6Devnics&8] &7The light is now " + (this.plugin.game.isRedLight ? "&cRed" : "&aGreen"));
-//    }
-//
-//    @Subcommand("end")
-//    @CommandPermission("red.use")
-//    public void End(CommandSender sender) {
-//        this.plugin.game.end();
-//        Util.send(sender, "&8[&6Devnics&8] &7Ending the game..");
-//    }
+
 }
